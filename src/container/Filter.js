b80c3fc9160d054a43530089f9c4ac144f258558
Osong Agberndifor <agberndifor7@gmail.com>
2020-11-16 10:38:09 +0100

    Add prop-validation for filterPokemon function

--- src/container/Filter.js
+++ src/container/Filter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 import { filterPokemons } from '../actions/index';
 import details from '../css/Details.module.css';
 
 const Filter = props => {
   const handleChange = e => {
-    // eslint-disable-next-line react/prop-types
     const { filterPokemons } = props;
     filterPokemons(e.target.value);
   };
@@ -21,6 +21,10 @@ const Filter = props => {
       />
     </div>
   );
+};
+
+Filter.propTypes = {
+  filterPokemons: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = dispatch => bindActionCreators({
